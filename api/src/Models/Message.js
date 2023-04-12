72e52b3e56b87429444955265ab5621fc65b6e58
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-04-12 15:45:03 -0300

    add model message

---
+++ api/src/Models/Message.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema(
+    {
+      content: {
+        type: String,
+        required: true,
+      },
+      user: {
+        type: String,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  export default model("Message", schema);
