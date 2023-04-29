6fbc1eb70d3f2d43c0162be18ac625b6f9199f07
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-04-28 22:33:19 -0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
       <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Chat/>} />
+            <Route path="/" element={<LoginForm/>} />
             <Route path='/login' element={<LoginForm/>} />
             <Route path='/singin' element={<SignupForm/>} />
           </Routes>
