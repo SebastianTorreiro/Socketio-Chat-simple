9f57d684c46c399c20863a021fd1df319c0c2c9e
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-05-01 20:35:39 -0300

    add user login & registered case

--- client/src/Reducer/index.js
+++ client/src/Reducer/index.js
@@ -1,11 +1,33 @@
+import {
+  GET_USER_REGISTERED,
+  GET_USER_LOGIN,
+  SET_ERROR_TRUE,
+} from "../Actions/constantes.js";
+
 const initialState = {
   user: {},
+  error: "",
 };
 
 export default function rootReducer(state = initialState, action) {
-  switch (action.payload) {
-    case '': {
-      return;
+  switch (action.type) {
+    case GET_USER_REGISTERED: {
+      return {
+        ...state,
+        user: action.payload,
+      };
+    }
+    case GET_USER_LOGIN: {
+      return {
+        ...state,
+        user: action.payload,
+      };
+    }
+    case SET_ERROR_TRUE: {
+      return {
+        ...state,
+        error: action.payload.response.data.error,
+      };
     }
     default:
       return { ...state };
