d42a50cc1c7834545756446a7da70af08bf85e33
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-04-30 21:45:29 -0300

    update controller & routes user

--- api/src/Controllers/User.controller.js
+++ api/src/Controllers/User.controller.js
@@ -1,14 +1,35 @@
+import { encrypt, compare } from "../Helpers/handleBcrypt.js";
+import User from "../Models/User.js";
 import UserService from "../Service/User.Service.js";
 
 const service = new UserService()
 
 
-export default async function createUser(req, res){
-    const {email, password} = req.body
+export default async function createUser(req, res){    
     try {
-        const createdUser = await service.createUser(email, password)
+        const {email, password, edad, nombre} = req.body
+        if(!email || !password || !edad || !nombre) return res.json({message: 'Error, no se enviaron los datos correspondientes'})
+        const passwordHash = await encrypt(password)
+        const createdUser = await service.createUser({email, password: passwordHash, edad, nombre})
         res.json({data:createdUser, status: 'OK'})
     } catch (error) {
         res.json({message: 'Error, no se pudo crear el usuario'})
     }
 };
+
+
+export default async function loginUser(req, res){
+    try {
+        const {email, password} = req.body
+        const userFind = User.findOne({email})
+        if(!userFind) return res.status(404).json({error:'No se a encontrado un usuario con ese email'})
+        const passwordCompare = compare(password, userFind.password)
+        console.log(passwordCompare)
+        if(!passwordCompare) return res.json({Error:'La constraseña es incorrecta'})
+        res.json(userFind)
+        
+    } catch (error) {
+         
+    }
+
+}
--- api/src/Routes/User.Router.js
+++ api/src/Routes/User.Router.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router()
 import createUser from '../Controllers/User.controller.js'
 
-router.post('/create', createUser)
+router.post('/singup', createUser)
 
+router.post('/login', loginUser);
 export default router
